e38a328ed357e8381e5b56298889daf0a0eb86b5
James Gurtowski <gurtowsk@yellowstone.cshl.edu>
2012-12-20 13:35:22 -0500

    added driver

--- getFastaLengths.js
+++ getFastaLengths.js
@@ -2,8 +2,12 @@
 
 load("fastaParser.js");
 
+if(arguments.length != 1){
+	print("getfastaLengths.js file.fa");	
+	quit();
+}
 
-var parser = jsb.fastaParser("test.fa");
+var parser = jsb.fastaParser(arguments[0]);
 
 var record = null;
 
