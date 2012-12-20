74c2178fe2c47eba25ecc25e39d5764ed269974c
James Gurtowski <gurtowsk@bnbdev1.blacknblue.cshl.edu>
2012-12-20 14:11:43 -0500

    added

--- getFastaLengths.js
+++ getFastaLengths.js
@@ -14,5 +14,5 @@ var record = null;
 var f = 0;
 while(parser.hasNext()){
     record = parser.next();
-    print(record.name + ":" + record.sequence.length());
+    print(record.name + " " + record.sequence.length());
 }
